4ddb1f0d2c28d47c84e5d3f09d92152cfe6fa0d8
Jon Arve Ovesen <jonovesen@gmail.com>
2025-07-22 17:45:16 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,1 @@
+const path = require('path');module.exports = {  mode: 'development',  entry: './src/frontend/app.ts',  module: {    rules: [      {        test: /\.ts$/,        use: 'ts-loader',        exclude: /node_modules/,      },    ],  },  resolve: {    extensions: ['.ts', '.js'],  },  output: {    filename: 'bundle.js',    path: path.resolve(__dirname, 'public', 'js'),  },};
